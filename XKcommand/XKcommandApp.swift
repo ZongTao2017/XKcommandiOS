e088741479fafae5daa78d69380eca151d4e6539
taozong <tzong1987@hotmail.com>
2021-11-23 14:40:31 -0800

    Initial Commit

---
+++ XKcommand/XKcommandApp.swift
@@ -0,0 +1,17 @@
+//
+//  XKcommandApp.swift
+//  XKcommand
+//
+//  Created by Tao Zong on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct XKcommandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
